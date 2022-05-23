e649f8a92b5e7984d528f7404f9ea020eac72ad9
Peter Delgado <peterrafaeldelgado@gmail.com>
2022-05-23 13:16:57 +0200

    Config of Button

--- APItest/SheetViewController.swift
+++ APItest/SheetViewController.swift
@@ -8,17 +8,37 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 @available(iOS 15.0, *)
 class SheetViewController: UIViewController, UISheetPresentationControllerDelegate {
+    
+    
+    @IBOutlet weak var newButton: UIButton!
+    
+    @IBAction func newButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    
     
     override var sheetPresentationController: UISheetPresentationController{
         presentationController as! UISheetPresentationController
     }
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+      
+//        parent?.view.addSubview(child.view)
+//        parent?.addChild(child)
+//        child.didMove(toParent: parent)
+//
+//
+        
         view.backgroundColor = .systemTeal
         
         sheetPresentationController.delegate = self
@@ -29,11 +49,47 @@ class SheetViewController: UIViewController, UISheetPresentationControllerDelega
                 .large()
         ]
         
+        let contentView = UIHostingController(rootView: SheetView())
+        addChild(contentView)
+        view.addSubview(contentView.view)
+        
+        contentView.view.translatesAutoresizingMaskIntoConstraints = false
+        contentView.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        contentView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        contentView.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        contentView.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        
         makeLabels()
+     //   makeLabels2()
     }
     
+//        fileprivate func setupContraints(){
+//        contentView.view.translatesAutoresizingMaskIntoConstraints = false
+//        contentView.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        contentView.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        contentView.view.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        contentView.view.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+//
+//            }
+    
     func makeLabels(){
-        let button = UIButton(configuration: .filled(), primaryAction:.init(handler:{ _ in
+        
+        
+    
+        //      setupContraints()
+//     button.translatesAutoresizingMaskIntoConstraints = false
+//        button.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 2).isActive = true
+//        button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -150).isActive = true
+//        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+
+       
+   
+    }
+    func makeLabels2(){
+        
+        let button2 = UIButton(configuration: .filled(), primaryAction:.init(handler:{ _ in
             
             let storyboard = UIStoryboard(name: "Sheet", bundle:nil)
             let sheetPresentationController =
@@ -43,20 +99,17 @@ class SheetViewController: UIViewController, UISheetPresentationControllerDelega
             
         }))
         
-        button.setTitle("Present Sheet", for: .normal)
-        button.configuration?.cornerStyle = .capsule
-        view.addSubview(button)
+        button2.setTitle("POST", for: .normal)
+        button2.configuration?.cornerStyle = .capsule
     
         //      setupContraints()
-//
-        button.translatesAutoresizingMaskIntoConstraints = true
-        button.leftAnchor.constraint(equalTo: 500).isActive = true
-        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 45).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 45).isActive = true
-   
+     button2.translatesAutoresizingMaskIntoConstraints = false
+        button2.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 3).isActive = true
+        button2.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -145).isActive = true
+        button2.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button2.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        
     }
-    
     
     
 }
